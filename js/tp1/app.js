0dbe90853f31bd15831973ff8daf44cc616cbbd6
Wilfried Noubissi <wilfried.noubissi06@gmail.com>
2021-09-30 16:59:41 +0200

    travaux pratiques js

---
+++ js/tp1/app.js
@@ -0,0 +1,5 @@
+alert("Bienvenue sur la page de formulaire");
+const variable1 = prompt("Entrez la 1ère variable");
+const variable2 = prompt("Entrez la 2ème variable");
+const sum = parseInt(variable1) + parseInt(variable2);
+alert("Le résultat est " + sum);
---
+++ js/tp2/app.js
@@ -0,0 +1,4 @@
+const distance = prompt("Entrez la distance en mètres");
+const temps = prompt("Entrez le temps en seconde");
+let vitesse = distance / temps;
+alert("la vitesse est " + vitesse + "m/s");
