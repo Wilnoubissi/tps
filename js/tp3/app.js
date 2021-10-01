999177bb028259f6e2683466d86480819d7faa83
Wilfried Noubissi <wilfried.noubissi06@gmail.com>
2021-10-01 11:30:33 +0200

    travaux pratiques js: categeories age

---
+++ js/tp3/app.js
@@ -0,0 +1,14 @@
+let age = parseInt(prompt("Ecrire son age"));
+if (age < 7 && age > 17) {
+  alert("Non repertorié");
+} else if (age >= 7 && age <= 9) {
+  alert("Poussin");
+} else if (age >= 10 && age <= 11) {
+  alert("Pupille");
+} else if (age >= 12 && age <= 13) {
+  alert("Benjamin");
+} else if (age >= 14 && age <= 15) {
+  alert("Minime");
+} else if (age >= 16 && age <= 17) {
+  alert("Cadet");
+}
